b9fc547def02eee44a597046a8a8dec09870a7e3
alicjaszweda <alicjadysk@gmail.com>
2021-03-18 21:17:00 +0100

    Initial commit

---
+++ src/app/datepicker-actions-example.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+/** @title Datepicker action buttons */
+@Component({
+  selector: 'datepicker-actions-example',
+  templateUrl: 'datepicker-actions-example.html',
+  styleUrls: ['datepicker-actions-example.css']
+})
+export class DatepickerActionsExample {}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
